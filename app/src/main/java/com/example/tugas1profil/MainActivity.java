941cee26aaa195d7eb3787d7cd0e7207d6234b9b
andrian-pe <56512118+andrian-pe@users.noreply.github.com>
2019-12-29 11:16:59 +0700

    localize

--- app/src/main/java/com/example/tugas1profil/MainActivity.java
+++ app/src/main/java/com/example/tugas1profil/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.tugas1profil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_setting=(Button)findViewById(R.id.btn_setting);
+
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+
+            }
+
+        });
     }
 }
